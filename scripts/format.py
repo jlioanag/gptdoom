b5017c62ca217a822db65d41fd90d711e64f096a
Julian L <jthemage101@gmail.com>
2020-09-17 14:33:43 -0400

    tensorflow training now works + full dataset

--- scripts/format.py
+++ scripts/format.py
@@ -5,13 +5,16 @@ import codecs
 with open('data/Lyrics_MFDOOM.json', 'r') as read_file:
     data = json.load(read_file)
     lyrics = []
+    # print(len(data['songs']))
     for song in data['songs']:
         lyrics.append(song['lyrics'])
-    test = data['songs'][0]['lyrics']
-    print([test])
+    # test = data['songs'][203]['lyrics']
+    # print([test])
+    # print(lyrics[203])
 
-with codecs.open('test.txt', 'w', encoding='utf8') as f:
+with codecs.open('data/Lyrics_MFDOOM.txt', 'w', encoding='utf8') as f:
     for item in lyrics:
-        if '[' not in item:
-            item += '\n'
-            f.write(item)
+        # if '[' not in item:
+        item.replace('[Instrumental]\n','')
+        item += '\n'
+        f.write(item)
